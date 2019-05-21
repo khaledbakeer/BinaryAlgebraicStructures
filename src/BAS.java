a6eb476245ab497364b046429118c987284b1313
khaledbakeer <bakeer@khaled.de>
2019-05-21 14:26:03 +0200

    add bitOf to convert from char ('1', '0') to boolean

--- src/BAS.java
+++ src/BAS.java
@@ -4,5 +4,14 @@
  */
 public class BAS {
 
-
+    /**
+     * bitOf:
+     * ------
+     *
+     * @param input char '1' or '0'.
+     * @return boolean value converted from char.
+     */
+    private static boolean bitOf(char input) {
+        return (input == '1');
+    }
 }
