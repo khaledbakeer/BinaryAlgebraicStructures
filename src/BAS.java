b8daf0596d857eeeb0444a2144bf9a05231fa3e9
khaledbakeer <bakeer@khaled.de>
2019-05-21 14:42:35 +0200

    add several Strings from type of binary digits using Algebraic Structures (Addition Operator).

--- src/BAS.java
+++ src/BAS.java
@@ -30,7 +30,7 @@ public class BAS {
     /**
      * Add Operator As Boolean:
      * ------------------------
-     *
+     * <p>
      * add several binary values from type char, and return boolean value.
      *
      * @param chars like '1', '0', '0', '1', ... ext.
@@ -59,5 +59,26 @@ public class BAS {
             tmp = tmp ^ bitOf(x);
         }
         return (tmp) ? '1' : '0';
+    }
+
+    /**
+     * Add:
+     * ----
+     * <p>
+     * add several Strings from type of binary digits using Algebraic Structures (Addition Operator).
+     *
+     * @param args Strings from type "101", "100", "111", ... ext.
+     * @return String from type "110".
+     */
+    private static String Add(String... args) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < args[0].length(); i++) {
+            char[] row = new char[args.length];
+            for (int j = 0; j < args.length; j++) {
+                row[j] = args[j].toCharArray()[i];
+            }
+            sb.append(AdditionOperatorAsChar(row));
+        }
+        return sb.toString();
     }
 }
